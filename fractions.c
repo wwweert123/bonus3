9b3d953582912fc71edd11010ed7dec2eb278b75
wwweert123 <puachengxuan.fp@gmail.com>
2023-09-12 16:50:06 +0800

    Initial commit

---
+++ fractions.c
@@ -0,0 +1,14 @@
+/**
+ * CS1010 Semester 1 AY23/24
+ * Bonus question By Frederick
+ *
+ * @file: board.c
+ * @author: XXX (Group YYY)
+ */
+
+#include "cs1010.h"
+
+int main()
+{
+    // TODO: Find the minimum number of moves needed to get all the figures into one cell
+}
